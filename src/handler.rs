4ec2f10727b8ca204263b805aaa7cd1ab0c93598
zuon <dev@dev.com>
2018-06-25 15:20:33 +0800

    实现了新的TopicHandler

--- src/handler.rs
+++ src/handler.rs
@@ -2,14 +2,13 @@ use std::sync::{Arc, RwLock};
 use std::sync::atomic::AtomicUsize;
 
 use fnv::FnvHashMap;
-use mqtt::handler::TopicHandle;
-use mqtt::session::Session;
 
 use pi_vm::adapter::JS;
 use pi_vm::pi_vm_impl::VMFactory;
 use pi_vm::bonmgr::{ptr_jstype, BON_MGR};
 use pi_db::mgr::Mgr;
 use pi_lib::atom::Atom;
+use pi_lib::handler::{Env, Handler, Args};
 
 /*
 * Topic处理器
@@ -21,20 +20,24 @@ pub struct TopicHandler {
 	gray_tab: 	Arc<RwLock<FnvHashMap<usize, (Arc<VMFactory>, Mgr)>>>,	//灰度表
 }
 
-impl TopicHandle for TopicHandler {
-	fn handle(&self, topic: Atom, _version: u8, session: Arc<Session>, bin: Arc<Vec<u8>>) {
-		let (factory, mgr) = self.get(session.clone());
+impl Handler for TopicHandler {
+	type HandleResult = ();
+
+	fn handle(&self, env: Arc<dyn Env>, topic: Atom, args: Args) -> Self::HandleResult {
+		let (factory, mgr) = self.get(env.clone());
         let topic_name = topic.clone();
-		let args = Box::new(move |vm: Arc<JS>| {
+		let real_args = Box::new(move |vm: Arc<JS>| {
 			vm.new_str((*topic_name).to_string());
+			let _vsn: &u8 = args.get_ref(0).unwrap();
+			let bin: &Arc<Vec<u8>> = args.get_ref(1).unwrap();
 			let array = vm.new_uint8_array(bin.len() as u32);
 			array.from_bytes(bin.as_slice());
 			let ptr = Box::into_raw(Box::new(mgr.clone())) as usize;
 			ptr_jstype(BON_MGR.objs.clone(), vm.clone(), ptr, 2976191628);
-			let ptr = Box::into_raw(Box::new(session.clone())) as usize;
+			let ptr = Box::into_raw(Box::new(env.clone())) as usize;
 			ptr_jstype(BON_MGR.objs.clone(), vm.clone(), ptr, 2256377725);
 		});
-		factory.call(0, args, Atom::from((*topic).to_string() + " rpc task"));
+		factory.call(0, real_args, Atom::from((*topic).to_string() + " rpc task"));
 	}
 }
 
@@ -84,9 +87,10 @@ impl TopicHandler {
 	}
 
 	//获取指定的虚拟机工厂和事务管理器
-	fn get(&self, session: Arc<Session>) -> (Arc<VMFactory>, Mgr) {
+	fn get(&self, session: Arc<Env>) -> (Arc<VMFactory>, Mgr) {
 		match session.get_attr(Atom::from("_$gray")) {
-			Some(vec) => {
+			Some(any) => {
+				let vec: &Vec<u8> = any.downcast_ref().unwrap();
                 let gray = usize::from_le(unsafe { *(vec[..].as_ptr() as *mut usize) });
 				match self.get_gray(gray) {
 					None => self.get_default(),
