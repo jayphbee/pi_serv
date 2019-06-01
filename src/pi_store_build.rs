a6f6d15c56ec759f7337c20a10191eb08dadcfca
jayphbee <jayphbee@163.com>
2019-06-01 20:41:40 +0800

    fix build error

--- src/pi_store_build.rs
+++ src/pi_store_build.rs
@@ -36,6 +36,27 @@ fn call_4027749383(js: Arc<JS>, v:Vec<JSType>) -> Option<CallResult>{
     Some(CallResult::Ok)
 }
 
+
+fn call_2763865666(js: Arc<JS>, v:Vec<JSType>) -> Option<CallResult>{
+	let param_error = "param error in new";
+
+	let jst0 = &v[0];
+    if !jst0.is_string(){ return Some(CallResult::Err(String::from(param_error)));}
+    let jst0 = Atom::from(jst0.get_str());
+
+
+	let jst1 = &v[1];
+	if !jst1.is_number(){ return Some(CallResult::Err(String::from(param_error)));}
+	let jst1 = jst1.get_u32() as usize;
+
+
+    let result = pi_store::file_mem_db::FileMemDB::new(jst0,jst1);
+    let ptr = Box::into_raw(Box::new(result)) as usize;let mut result = ptr_jstype(js.get_objs(), js.clone(), ptr,2325173571);
+
+
+    Some(CallResult::Ok)
+}
+
 fn drop_568147534(ptr: usize){
     unsafe { Box::from_raw(ptr as *mut pi_store::lmdb_file::DB) };
 }
@@ -47,4 +68,5 @@ pub fn register(mgr: &BonMgr){
     mgr.regist_struct_meta(StructMeta{name:String::from("pi_store::lmdb_file::DB"), drop_fn: drop_568147534}, 568147534);
     mgr.regist_struct_meta(StructMeta{name:String::from("pi_store::file_mem_db::FileMemDB"), drop_fn: drop_2325173571}, 2325173571);
     mgr.regist_fun_meta(FnMeta::CallArg(call_4027749383), 4027749383);
+    mgr.regist_fun_meta(FnMeta::CallArg(call_2763865666), 2763865666);
 }
